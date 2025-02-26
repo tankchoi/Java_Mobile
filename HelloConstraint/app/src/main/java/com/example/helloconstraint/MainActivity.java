bbcc5e1faf723c2a76e662525624a06bba52194b
tankchoi <tadzltv22082004@gmail.com>
2025-02-27 02:50:57 +0700

    Homework 1.2B

---
+++ HelloConstraint/app/src/main/java/com/example/helloconstraint/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.helloconstraint;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private int mCount;
+    private Button btnToast;
+    private Button btnCount;
+    private Button btnZero;
+    private TextView txtCount;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        btnToast = (Button) findViewById(R.id.button_toast);
+        btnCount = (Button) findViewById(R.id.button_count);
+        btnZero = (Button) findViewById(R.id.button_zero);
+        txtCount = (TextView) findViewById(R.id.show_count);
+
+        btnToast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(MainActivity.this, R.string.toast_message, Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+        btnCount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ++mCount;
+                btnZero.setBackgroundColor(Color.parseColor("#E91E63"));
+                if (mCount % 2 == 0) {
+                    btnCount.setBackgroundColor(Color.parseColor("#4CAF50"));
+                } else {
+                    btnCount.setBackgroundColor(Color.parseColor("#F44336"));
+                }
+                if (txtCount != null) {
+                    txtCount.setText(Integer.toString(mCount));
+                }
+            }
+        });
+        btnZero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnZero.setBackgroundColor(Color.parseColor("#ABABAB"));
+                mCount = 0;
+                txtCount.setText(Integer.toString(mCount));
+            }
+        });
+
+
+    }
+}
